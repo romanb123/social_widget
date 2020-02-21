323fa69aa704f406173c7dc0d5db76d31051f037
romanb123 <44100462+romanb123@users.noreply.github.com>
2020-02-21 23:13:56 +0200

    vers3

--- includes/social_wid_scripts.php
+++ includes/social_wid_scripts.php
@@ -2,8 +2,8 @@
 
 function sosial_wid_scripts()
 {
-    wp_enqueue_style('main style', plugins_url() . '/SOCIAL_WIDGET/css/main.css');
-    wp_enqueue_script('main js', plugins_url() . '/SOCIAL_WIDGET/js/main.js');
+    wp_enqueue_style('main style', plugins_url() . '/social_widget-master/css/main.css');
+    wp_enqueue_script('main js', plugins_url() . '/social_widget-master/js/main.js');
 }
 
 add_action('wp_enqueue_scripts', 'sosial_wid_scripts');
