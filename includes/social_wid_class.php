45cff6c9a13b231cd198f677c7053c8fd73f8fb9
romanb123 <44100462+romanb123@users.noreply.github.com>
2020-02-21 23:11:14 +0200

    vers2

--- includes/social_wid_class.php
+++ includes/social_wid_class.php
@@ -15,18 +15,76 @@ class Social_Widget extends WP_Widget
     public function widget($args, $instance)
     {
         ?>
-       test
+        <?php echo ($args['before_widget'])?>
+        <br>
+        
+       <a href="<?php echo esc_attr($instance['facebook'])?>"><span class="dashicons dashicons-facebook"></span></a>
+       <a href="<?php echo esc_attr($instance['instagram'])?>"><span class="dashicons dashicons-instagram"></span></a>
+       <a href="<?php echo esc_attr($instance['twitter'])?>"><span class="dashicons dashicons-twitter"></span></a>
+       <?php echo ($args['before_widget'])?>
        <?php
 }
 
     public function form($instance)
     {
-
+        $this->getForm($instance);
     }
 
     public function update($new_instance, $old_instance)
     {
-        // processes widget options to be saved
+        $instance = array();
+        $instance['facebook'] = (!empty($new_instance['facebook'])) ? sanitize_text_field($new_instance['facebook']) : '';
+        $instance['instagram'] = (!empty($new_instance['instagram'])) ? sanitize_text_field($new_instance['instagram']) : '';
+        $instance['twitter'] = (!empty($new_instance['twitter'])) ? sanitize_text_field($new_instance['twitter']) : '';
+
+        return $instance;
+
     }
+
+    public function getForm($instance)
+    {
+        if ($instance) {
+            $facebook = esc_attr($instance['facebook']);
+        } else {
+            $facebook = '';
+        }
+
+        if ($instance) {
+            $instagram = esc_attr($instance['instagram']);
+        } else {
+            $instagram = '';
+        }
+
+        if ($instance) {
+            $twitter = esc_attr($instance['twitter']);
+        } else {
+            $twitter = '';
+        }
+
+        ?>
+    <h4>Facebook</h4>
+    <p>
+    <label for="<?php echo $this->get_field_id('facebook'); ?>"><?php _e('Link to your facebook:');?></label>
+        <input class="widefat" id="<?php echo $this->get_field_id('facebook'); ?>"
+         name="<?php echo $this->get_field_name('facebook'); ?>"
+         type="text" value="<?php echo esc_attr($facebook); ?>">
+    </p>
+
+    <h4>instagram</h4>
+    <p>
+    <label for="<?php echo $this->get_field_id('instagram'); ?>"><?php _e('Link to your instagram:');?></label>
+        <input class="widefat" id="<?php echo $this->get_field_id('instagram'); ?>"
+         name="<?php echo $this->get_field_name('instagram'); ?>"
+         type="text" value="<?php echo esc_attr($instagram); ?>">
+    </p>
+
+    <h4>twitter</h4>
+    <p>
+    <label for="<?php echo $this->get_field_id('twitter'); ?>"><?php _e('Link to your twitter:');?></label>
+        <input class="widefat" id="<?php echo $this->get_field_id('twitter'); ?>"
+         name="<?php echo $this->get_field_name('twitter'); ?>"
+         type="text" value="<?php echo esc_attr($twitter); ?>">
+    </p>
+<?php
 }
-?>
+}
--- includes/social_wid_scripts.php
+++ includes/social_wid_scripts.php
@@ -3,7 +3,7 @@
 function sosial_wid_scripts()
 {
     wp_enqueue_style('main style', plugins_url() . '/SOCIAL_WIDGET/css/main.css');
-    wp_enqueue_scripts('main js', plugins_url() . '/SOCIAL_WIDGET/js/main.js');
+    wp_enqueue_script('main js', plugins_url() . '/SOCIAL_WIDGET/js/main.js');
 }
 
 add_action('wp_enqueue_scripts', 'sosial_wid_scripts');
